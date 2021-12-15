f76e1c360acaed3ce868eba9e6aaa8e88ec97cf9
Анжела <anzhela.namistyuk@yandex.ru>
2021-12-15 15:49:29 +0300

    add GenreTitle

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -21,13 +21,39 @@ class Genres(models.Model):
 class Titles(models.Model):
     name = models.CharField(max_length=256)
     year = models.IntegerField()
-    description = models.TextField(blank=True, null=True, )
-    genre = models.ManyToManyField(Genres)
+    description = models.TextField(blank=True, null=True,)
+    genre = models.ManyToManyField(
+        Genres,
+        through='GenreTitle',
+        through_fields=('title', 'genre'))
     category = models.ForeignKey(
-        Categories, related_name='categories', on_delete=models.SET_NULL, null=True)
+        Categories,
+        related_name='categories',
+        on_delete=models.SET_NULL,
+        null=True)
+    rating = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+
+class GenreTitles(models.Model):
+    title = models.ForeignKey(
+        Titles,
+        on_delete=models.SET_NULL,
+        null=True)
+    genre = models.ForeignKey(
+        Genres,
+        on_delete=models.SET_NULL,
+        null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'genre'],
+                name='unique_title_genre'
+            )
+        ]
 
 
 class Review(models.Model):
