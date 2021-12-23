51a8336c91ae9be1d6191d194fe4db8688987b5b
xrito <88393376+xrito@users.noreply.github.com>
2021-12-23 22:46:25 +0400

    Rev 2.1

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
         ordering = ('id',)
-        
+
     def __str__(self):
         return self.name
 
